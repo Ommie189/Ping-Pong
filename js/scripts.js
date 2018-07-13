f501f5a561dd7d67beb857949cdb0f5d675df24e
Ommie189 <naomikuhi189@gmail.com>
2018-07-13 14:59:02 +0300

    worked on user interface

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 //business logic
-var Number = function(change) {
+var number = function(change) {
   var result = [];
   for (var i = 1; i <= change; i++) {
     if (i % 15 === 0) {
@@ -15,3 +15,15 @@ var Number = function(change) {
   return result;
 };
 // USER INTERFACE
+$(document).ready(function(){
+  $("form#userInput").submit(function(event){
+    $("#result").empty();
+    var change =parseInt($("input#change").val());
+    var output =number(change);
+
+    output.forEach(function(number){
+      $("#result").append('<li>'+ number + '</li>');
+    });
+    event.preventDefault();
+  });
+});
